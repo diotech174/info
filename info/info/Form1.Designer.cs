b79e74b0973524531b25061cdbfa85fc5029ca19
diotech174 <73372540+diotech174@users.noreply.github.com>
2021-12-08 16:48:26 -0300

    first send

---
+++ info/info/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace info
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Inicio");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tvwTitulos = new System.Windows.Forms.TreeView();
+            this.txtManual = new System.Windows.Forms.RichTextBox();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvwTitulos
+            // 
+            this.tvwTitulos.Location = new System.Drawing.Point(12, 50);
+            this.tvwTitulos.Name = "tvwTitulos";
+            treeNode2.Name = "Inicio";
+            treeNode2.Text = "Inicio";
+            this.tvwTitulos.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.tvwTitulos.Size = new System.Drawing.Size(281, 615);
+            this.tvwTitulos.TabIndex = 0;
+            this.tvwTitulos.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwTitulos_AfterSelect);
+            // 
+            // txtManual
+            // 
+            this.txtManual.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtManual.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtManual.Location = new System.Drawing.Point(299, 19);
+            this.txtManual.Name = "txtManual";
+            this.txtManual.ReadOnly = true;
+            this.txtManual.Size = new System.Drawing.Size(805, 646);
+            this.txtManual.TabIndex = 1;
+            this.txtManual.Text = resources.GetString("txtManual.Text");
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(12, 20);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(236, 23);
+            this.txtPesquisa.TabIndex = 2;
+            this.txtPesquisa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPesquisa_KeyPress);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("btnPesquisar.Image")));
+            this.btnPesquisar.Location = new System.Drawing.Point(254, 19);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(39, 25);
+            this.btnPesquisar.TabIndex = 3;
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1116, 677);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.txtManual);
+            this.Controls.Add(this.tvwTitulos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Central de Ajuda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvwTitulos;
+        private System.Windows.Forms.RichTextBox txtManual;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button btnPesquisar;
+    }
+}
+
---
+++ info/info/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace info
+{
+    public partial class Form1 : Form
+    {
+        private string text_inicio = "";
+        private string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.text_inicio = txtManual.Text;
+            try
+            {
+                string[] dirs = Directory.GetDirectories(RunningPath+"/docs/", "*");
+
+                int i = 1;
+                foreach (string dir in dirs)
+                {
+                    tvwTitulos.Nodes.Add(dir.Replace(RunningPath + "/docs/", ""));
+
+                    string[] files = Directory.GetFiles(dir, "*");
+                    foreach (string file in files)
+                    {
+                        tvwTitulos.Nodes[i].Nodes.Add("* " + file.Replace(dir+@"\", "").Replace(".txt", ""));
+                    }
+
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tvwTitulos_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string node_text = tvwTitulos.SelectedNode.Text;
+
+            if (node_text == "Inicio")
+            {
+                txtManual.Text = this.text_inicio;
+            }
+            else
+            {
+                if (node_text.IndexOf("*") != -1)
+                {
+                    string[] dirs = Directory.GetDirectories(RunningPath + "/docs/", "*");
+
+                    foreach (string dir in dirs)
+                    {
+                        string[] files = Directory.GetFiles(dir, "*");
+                        foreach (string file in files)
+                        {
+                            string filename = file.Replace(dir, "").Replace("\\", "").Replace(".txt", "");
+                            string node_selected = node_text.Replace("* ", "");
+
+                            if (filename == node_selected)
+                            {
+                                foreach (Control item in txtManual.Controls.OfType<LinkLabel>().ToList())
+                                {
+                                    txtManual.Controls.Remove(item);
+                                }
+
+                                txtManual.Text = getTextFile(file);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string getTextFile(string fileName)
+        {
+            string text = "";
+
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(fileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    text += line + "\n";
+                }
+            }
+
+            return text;
+        }
+
+        private void Pesquisar()
+        {
+            string pesquisa = txtPesquisa.Text;
+
+            ArrayList resultados = new ArrayList();
+
+            string[] dirs = Directory.GetDirectories(RunningPath + "/docs/", "*");
+
+            foreach (string dir in dirs)
+            {
+                string[] files = Directory.GetFiles(dir, "*");
+                foreach (string file in files)
+                {
+                    string filename = file.Replace(dir, "").Replace("\\", "").Replace(".txt", "");
+
+                    if (filename.ToLower().IndexOf(pesquisa.ToLower()) != -1)
+                    {
+                        string[] results = new string[] {
+                            "* " + filename,
+                            file
+                        };
+
+                        resultados.Add(results);
+                    }
+                    else
+                    {
+                        string text = getTextFile(file);
+
+                        if (text.ToLower().IndexOf(pesquisa.ToLower()) != -1)
+                        {
+                            string[] results = new string[] {
+                            "* " + filename,
+                            file
+                        };
+
+                            if (!resultados.Contains(results))
+                                resultados.Add(results);
+                        }
+                    }
+                }
+            }
+
+            if (resultados.Count == 0)
+            {
+                foreach (Control item in txtManual.Controls.OfType<LinkLabel>().ToList())
+                {
+                    txtManual.Controls.Remove(item);
+                }
+
+                txtManual.Text = "\n\tNenhum resultado encontrado!";
+            }
+            else
+            {
+                foreach (Control item in txtManual.Controls.OfType<LinkLabel>().ToList())
+                {
+                    txtManual.Controls.Remove(item);
+                }
+
+                txtManual.Text = "\n\tRESULTADOS (" + resultados.Count + "):\n\n";
+                foreach (string[] item in resultados)
+                {
+            
+                    LinkLabel link = new LinkLabel();
+                    link.Text = item[0];
+                    link.AutoSize = true;
+
+                    link.Location = txtManual.GetPositionFromCharIndex(txtManual.TextLength);
+                    txtManual.Controls.Add(link);
+
+                    txtManual.AppendText(link.Text + "   ");
+                    txtManual.SelectionStart = txtManual.TextLength;
+
+                    txtManual.Text += "\n\n"+getTextFile(item[1])+"\n\n";
+                }
+            }
+
+            txtPesquisa.Clear();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            if(txtPesquisa.Text.Trim() != "")
+            {
+                Pesquisar();
+            }
+            else
+            {
+                MessageBox.Show("Digite sua pesquisa", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPesquisa.Focus();
+            }
+        }
+
+        private void txtPesquisa_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13)
+            {
+                if (txtPesquisa.Text.Trim() != "")
+                {
+                    Pesquisar();
+                }
+                else
+                {
+                    MessageBox.Show("Digite sua pesquisa", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtPesquisa.Focus();
+                }
+            }
+        }
+    }
+}
